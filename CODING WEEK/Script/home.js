5cde252aadc47fc5a55e4e25c726e05c61e31abe
Avenez <145536778+Avenez@users.noreply.github.com>
2023-11-15 20:10:24 +0100

    Update home.js

--- CODING WEEK/Script/home.js
+++ CODING WEEK/Script/home.js
@@ -1,3 +1,22 @@
+let button = document.getElementsByTagName('button');
+let checkBox = document.getElementsByTagName('input');
+
+button.disabled = true;
+const illumina = function(){
+    button[0].classList.toggle('buttonCheked');
+    button.disabled = false;
+}
+
+const spegni = function(){
+    button[0].classList.remove('buttonCheked');
+    checkBox[4].checked = false;
+}
+
+window.onload = (e) =>{
+    spegni();
+};
+
+
 document.getElementsByTagName("button").addEventListner("click", function (e) {
     document.getElementsByTagName("form").submit();
 })
